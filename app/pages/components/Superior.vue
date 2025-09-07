c17159981857aacb25d00734c8cc6a3b7c8e5a52
ILHA SERVICE - Leonardo Pereira Pereira <leonardo.pereira.3@ebserh.gov.br>
2025-09-07 08:48:02 -0300

    superior component: layout ok, add more details

--- app/pages/components/Superior.vue
+++ app/pages/components/Superior.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="superior-section">
-    <div class="col-md-12 image-column"></div>
+    <div class="image-column"></div>
     <div class="container-fluid superior-container">
       <div class="row align-items-center h-100">
-        <div class="col-md-12 form-col">
-          <div class="form-content col-md-4">
-            <img src="../../../public/logo.png" alt="Logo" />
+        <div class="col-sm-6 col-md-12 form-col">
+          <div class="form-content col-sm-6 col-md-12">
+            <img src="/logo.png" class="logo" alt="Logo" />
             <h1 id="title">Aqui vai para um título persuasivo</h1>
             <p>Aqui pode ir uma subheadline mais explicativa</p>
 
@@ -28,20 +28,29 @@
 
 <style scoped>
 .superior-section {
-  height: 70vh;
+  height: 60vh;
 }
 
 .superior-container {
   display: flex;
   justify-content: center;
   position: relative;
-  top: -65vh;
+  top: -58vh;
   height: auto;
-  width: 70vh;
+  width: 90%;
   background-color: rgb(255, 255, 255);
   border-radius: none;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  padding: 20px;
+  padding: 30px;
+}
+@media (min-width: 768px) {
+  /* media query para a versão mobile ficar com o tamanho certo dos itens */
+  .superior-container {
+    height: auto;
+    width: 35%;
+    right: 20%;
+    padding: 50px;
+  }
 }
 .row {
   height: 100%;
@@ -49,20 +58,41 @@
 
 .image-column {
   height: 100%;
+  width: 100%;
+
   background-image: url("../../../public/fundo.png");
   background-size: cover;
-
-  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
 }
+
 .form-content {
   width: 100%;
-  max-width: 4000px;
+  max-width: 400px;
+  font-size: large;
 }
 #title {
   font-size: 22px;
   font-style: bold;
   color: #000;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
   margin-left: 5%;
+}
+
+#email,
+#wpp {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 25px;
+  text-align: left;
+}
+
+.logo {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40vh;
 }
 </style>
